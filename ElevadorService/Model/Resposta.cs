b65947ece6617b218838170155a7faf93fc54d4b
cadu0794 <cedusantos10@gmail.com>
2024-08-14 12:07:02 -0300

    Criando o fluxo de leitura do json para gerar os relatórios solicitados

---
+++ ElevadorService/Model/Resposta.cs
@@ -0,0 +1,9 @@
+﻿namespace ElevadorServices.Model
+{
+    class Resposta
+    {
+        public int andar { get; set; }
+        public char elevador { get; set; }
+        public char turno { get; set; }
+    }
+}
